9c845548c30b68f460b55f918a1a0910f3e05c1e
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-13 19:10:09 +0200

    Réalisation de la file d'attente

---
+++ 444flox-portfolio-front/src/components/queue/QueueCover.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const props = defineProps(['cover', 'title', 'subTitle'])
+</script>
+
+<template>
+  <div class="cover column">
+    <img class="project-in-queue" :src="'./covers/' + cover + '-cover.png'" alt="queued project cover" draggable="false">
+    <div class="title-container column flex-centered">
+      <h1 class="project-title">{{ title }}</h1>
+      <h2 class="project-type">{{ subTitle }}</h2>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+h1, h2 {
+  font-family: Modeseven, serif;
+
+}
+
+.cover {
+  justify-content: center;
+  height: 95%;
+}
+
+.project-in-queue {
+  width: min(15vh, 10vw);
+  object-fit: contain;
+  filter: drop-shadow(1px 1px 4px rgba(0,0,0, 0.35));
+}
+
+.project-title {
+  text-transform: uppercase;
+  color: var(--text-header-c);
+  font-size: max(2vh, min(2.5vw, 3vh));
+  filter: drop-shadow(1px 1px 4px rgba(0,0,0, 0.3));
+
+}
+
+.project-type {
+  font-size: max(1vh, min(1vw, 2vh));
+  color: var(--text-hover-header-c);
+  filter: drop-shadow(1px 1px 3px rgba(0,0,0, 0.4));
+  line-height: 0;
+}
+
+.title-container {
+  justify-content: space-between;
+}
+
+@media screen and (max-width: 850px) {
+
+  .project-in-queue {
+    width: min(20vh, 17.5vw);
+    min-width: 65px;
+  }
+}
+
+</style>
---
+++ 444flox-portfolio-front/src/components/queue/WaitingQueue.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import ProjectCover from "@/components/drive/ProjectCover.vue";
+import QueuCover from "@/components/queue/QueueCover.vue";
+</script>
+
+<template>
+  <h1>Waiting queue</h1>
+  <div class="list row">
+    <QueuCover class="cover column flex-centered" :cover="'sage'" :title="'sage'" :sub-title="'444flox'"/>
+  </div>
+
+</template>
+
+<style scoped>
+  h1 {
+    margin-top: 6.5%;
+    font-size: max(2.25vh, 1.5vw);
+    line-height: 100%;
+    color: var(--text-header-c);
+  }
+
+  .list {
+    height: calc(83% - 0.75vw);
+    flex-wrap: nowrap;
+    overflow-y: scroll;
+    scroll-behavior: smooth;
+    scrollbar-color: rgba(75,75,75, 0.75) transparent;
+    scrollbar-width: thin;
+    scrollbar-gutter: stable;
+  }
+
+  .cover {
+    margin: 0 3vh;
+  }
+
+  @media screen and (max-width: 850px) {
+    h1 {
+      font-size: max(2.75vh, 3.5vw);
+      margin-top: min(3vh, 5vw);
+    }
+
+    .cover {
+      margin: 0 1vh;
+    }
+
+    .list {
+      height: 80%;
+    }
+
+  }
+</style>
