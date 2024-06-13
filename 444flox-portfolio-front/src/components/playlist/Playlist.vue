0328449eaea9c89c456c35f4801d00a8bab04081
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-13 23:31:43 +0200

    Réalisation du conteneur playlist

---
+++ 444flox-portfolio-front/src/components/playlist/Playlist.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import PlaylistElement from "@/components/playlist/PlaylistElement.vue";
+import {projects} from "@/modules/utils/projects";
+
+</script>
+
+<template>
+  <h1>Playlist of user</h1>
+  <div class="list column">
+    <PlaylistElement v-for="project of projects" class="cover" :cover="project.coverName" :title="project.title" :sub-title="project.author + ', ' + project.category"/>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  margin: 1% 0;
+  font-size: max(2.25vh, 1.5vw);
+  line-height: 100%;
+  color: var(--text-header-c);
+}
+.list {
+  width: 100%;
+  height: 100%;
+  overflow-x: scroll;
+}
+
+
+</style>
---
+++ 444flox-portfolio-front/src/components/playlist/PlaylistElement.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+  const props = defineProps(['cover', 'title', 'subTitle'])
+</script>
+
+<template>
+  <div class="content row">
+    <img class="project-cover user-unselect-any" :src="'./covers/' + cover + '-cover.png'" alt="queued project cover" draggable="false">
+    <div class="title-container column">
+      <h1 class="project-title" ref="titleElem">{{ title }}</h1>
+      <h2 class="project-type" ref="subTitleElem">{{ subTitle }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  width: 100%;
+  height: 7.5vh;
+  align-items: center;
+  padding: 0.25% 0.25%;
+}
+
+h1, h2 {
+  font-family: Modeseven, serif;
+  text-align: left;
+}
+
+.project-cover {
+  height: 95%;
+  object-fit: contain;
+  border-radius: 5px;
+  filter: drop-shadow(1px 1px 4px rgba(0,0,0, 0.3));
+}
+
+.project-title {
+  text-transform: uppercase;
+  color: var(--text-header-c);
+  font-size: max(1.5vh, min(1.5vw, 2.25vh));
+  filter: drop-shadow(1px 1px 4px rgba(0,0,0, 0.3));
+}
+
+.project-type {
+  font-size: max(1vh, min(1vw, 2vh));
+  color: var(--text-hover-header-c);
+  filter: drop-shadow(1px 1px 3px rgba(0,0,0, 0.4));
+}
+
+.title-container {
+  height: 100%;
+  justify-content: center;
+  line-height: 1.35em;
+  margin: 0 2%;
+}
+</style>
