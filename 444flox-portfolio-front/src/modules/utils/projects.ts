3f4ae4dc5e3c5a60f2fc8ea40331dfff64599d27
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-13 19:50:57 +0200

    Add liste des projets

---
+++ 444flox-portfolio-front/src/modules/utils/projects.ts
@@ -0,0 +1,48 @@
+
+
+export interface Project {
+    title: string;
+    coverName: string;
+    author: string;
+    category: string;
+
+}
+
+export const projects: Project[] = [
+    {
+        title: 'Sage',
+        coverName: 'sage',
+        author: '444flox',
+        category: 'artistic direction',
+    },
+    {
+        title: 'Gen',
+        coverName: 'gen',
+        author: '444flox',
+        category: 'ui/ux',
+    },
+    {
+        title: 'Fallen hero',
+        coverName: 'fallen_hero',
+        author: '444flox',
+        category: 'artistic direction',
+    },
+    {
+        title: 'Cavalry',
+        coverName: 'cavalry',
+        author: '444flox',
+        category: 'edition',
+    },
+    {
+        title: 'Dispositifs',
+        coverName: 'dispositifs',
+        author: '444flox',
+        category: 'numeric project',
+    },
+    {
+        title: 'Posters',
+        coverName: 'posters',
+        author: '444flox',
+        category: 'visual creation',
+    }
+]
