4bccd9d8147f750a038fad1a352043cfb2cb2067
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-14 00:47:27 +0200

    Titre du projet clickable en playlist pour aller directement sur la page du projet

--- 444flox-portfolio-front/src/components/playlist/PlaylistElement.vue
+++ 444flox-portfolio-front/src/components/playlist/PlaylistElement.vue
@@ -6,7 +6,7 @@
   <div class="content row">
     <img class="project-cover user-unselect-any" :src="'./covers/' + cover + '-cover.png'" alt="queued project cover" draggable="false">
     <div class="title-container column">
-      <h1 class="project-title" ref="titleElem">{{ title }}</h1>
+      <RouterLink :to="'/' + cover.toString().toLocaleLowerCase()" class="project-title" ref="titleElem">{{ title }}</RouterLink>
       <h2 class="project-type" ref="subTitleElem">{{ subTitle }}</h2>
     </div>
   </div>
@@ -20,7 +20,7 @@
   padding: 0.25% 0.25%;
 }
 
-h1, h2 {
+a, h2 {
   font-family: Modeseven, serif;
   text-align: left;
   white-space: nowrap;
@@ -38,6 +38,10 @@ h1, h2 {
   color: var(--text-header-c);
   font-size: max(2.35vh, min(1.5vw, 2.25vh));
   filter: drop-shadow(1px 1px 4px rgba(0,0,0, 0.3));
+}
+
+.project-title:hover {
+  text-decoration: underline;
 }
 
 .project-type {
