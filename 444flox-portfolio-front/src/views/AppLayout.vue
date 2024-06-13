c7302439c96508fdd7a4fca11120525e0d184809
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-13 03:12:41 +0200

    Création d'un layout intégrant dans le header la navbar

---
+++ 444flox-portfolio-front/src/views/AppLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <header class="row">
+    <Navbar/>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  width: 100vw;
+  height: 7.5vh;
+  background: var(--header-c);
+  align-items: center;
+}
+
+main {
+  width: 100vw;
+  height: 92.5vh;
+}
+
+
+</style>
