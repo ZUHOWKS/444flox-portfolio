177a399a4192b11702c09ed7ae335f5e6c7549c1
ZUHOWKS <jojo.vilard38@gmail.com>
2024-06-19 00:05:19 +0200

    Responsive mobile cavalry project

--- 444flox-portfolio-front/src/views/projects/Cavalry.vue
+++ 444flox-portfolio-front/src/views/projects/Cavalry.vue
@@ -10,24 +10,27 @@
 </script>
 
 <template>
-  <section class="banner list">
-    <img src="@/assets/img/cavalry/instagram-1.png" alt="Instagram post 1 cavalry" rel="preload" draggable="false">
-  </section>
+  <div class="gallery">
+    <section class="banner list">
+      <img src="@/assets/img/cavalry/instagram-1.png" alt="Instagram post 1 cavalry" rel="preload" draggable="false">
+    </section>
 
-  <section class="covers row list">
-    <img src="@/assets/img/cavalry/cover.png" alt="Cover cavalry" rel="preload" draggable="false">
-    <img src="@/assets/img/cavalry/cover-back.png" alt="Cover-back cavalry" rel="preload" draggable="false">
-  </section>
+    <section class="covers row list">
+      <img src="@/assets/img/cavalry/cover.png" alt="Cover cavalry" rel="preload" draggable="false">
+      <img src="@/assets/img/cavalry/cover-back.png" alt="Cover-back cavalry" rel="preload" draggable="false">
+    </section>
 
-  <section class="posts row list">
-    <div class="row">
-      <img src="@/assets/img/cavalry/instagram-2.png" alt="Instagram post 2 cavalry" rel="preload" draggable="false">
-    </div>
-    <div class="column">
-      <img src="@/assets/img/cavalry/instagram-3.png" alt="Instagram post 3 cavalry" rel="preload" draggable="false">
-      <img src="@/assets/img/cavalry/instagram-4.png" alt="Instagram post 4 cavalry" rel="preload" draggable="false">
-    </div>
-  </section>
+    <section class="posts row">
+      <div class="row list">
+        <img src="@/assets/img/cavalry/instagram-2.png" alt="Instagram post 2 cavalry" rel="preload" draggable="false">
+      </div>
+      <div class="column list">
+        <img src="@/assets/img/cavalry/instagram-3.png" alt="Instagram post 3 cavalry" rel="preload" draggable="false">
+        <img src="@/assets/img/cavalry/instagram-4.png" alt="Instagram post 4 cavalry" rel="preload" draggable="false">
+      </div>
+    </section>
+  </div>
+
 
 </template>
 
@@ -38,6 +41,8 @@
 
   section {
     margin-bottom: 2vh;
+    margin-left: 2vh;
+    padding: 0 2vh;
     width: calc(100% - 2vh);
   }
 
@@ -58,8 +63,7 @@
   }
 
   .posts {
-    height: calc(100%);
-
+    flex-wrap: wrap;
   }
 
   .posts>.row {
@@ -83,5 +87,57 @@
     height: calc(50% - 1vh);
   }
 
+  @media screen and (max-width: 1775px) {
+    .posts>.row {
+      width: 100%;
+      margin-bottom: 2vh;
+    }
+
+    .posts>.column {
+      width: 100%;
+      overflow-x: scroll;
+      flex-direction: row;
+    }
+
+    .posts>.column>img {
+      height: 80vh;
+    }
+
+    .posts>.column>img:nth-child(1) {
+      margin-right: 2vh;
+    }
+  }
+
+  @media screen and (max-width: 1020px) {
+    .gallery {
+      width: 70vw;
+    }
+    .covers, .banner {
+      width: 100%;
+      overflow-x: scroll;
+    }
+
+    .banner>img {
+      width: unset;
+      height: 60vh;
+      margin-right: 4vh;
+    }
+
+    .covers>img {
+      width: 75vw;
+      margin-right: 2vh;
+    }
+
+    .posts>.row {
+      width: 100vw;
+      height: 100%;
+      overflow-x: scroll;
+    }
+
+    .posts>.row>img {
+      width: 100vw;
+
+    }
+  }
 
 </style>
